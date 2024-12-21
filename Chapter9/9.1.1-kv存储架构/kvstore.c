8821ff9256b134104085f65c250be85c8d2eb3b9
Bssn520 <Bssn520@duck.com>
2024-12-21 15:13:19 +0800

    feat: 增加CMD适配红黑树接口层 增加init_engine函数以选择不同引擎

--- Chapter9/9.1.1-kv存储架构/kvstore.c
+++ Chapter9/9.1.1-kv存储架构/kvstore.c
@@ -9,15 +9,24 @@
 #define KVSTORE_MAX_TOKENS 128
 
 // 定义协议中使用的命令
-const char *commands[] = {"SET", "GET", "DEL", "MOD"};
+const char *commands[] = {
+    "SET", "GET", "DEL", "MOD", "COUNT",
+    "RSET", "RGET", "RDEL", "RMOD", "RCOUNT"};
 enum
 {
     KVS_CMD_START = 0,
     KVS_CMD_SET = KVS_CMD_START,
-    KVS_CMD_GET = 1,
-    KVS_CMD_DEL = 2,
-    KVS_CMD_MOD = 3,
-    KVS_CMD_COUNT = 4
+    KVS_CMD_GET,
+    KVS_CMD_DEL,
+    KVS_CMD_MOD,
+    KVS_CMD_COUNT,
+
+    KVS_CMD_RSET,
+    KVS_CMD_RGET,
+    KVS_CMD_RDEL,
+    KVS_CMD_RMOD,
+    KVS_CMD_RCOUNT,
+    KVS_CMD_END
 };
 
 void *kvstore_malloc(size_t size)
@@ -32,6 +41,35 @@ void kvstore_free(void *ptr)
         free(ptr);
     }
 }
+
+#if ENABLE_RBTREE_KVENGINE
+
+int kvstore_rbtree_set(char *key, char *value)
+{
+    return rbtree_set(&tree, key, value);
+}
+
+char *kvstore_rbtree_get(char *key)
+{
+    return rbtree_get(&tree, key);
+}
+
+int kvstore_rbtree_del(char *key)
+{
+    return rbtree_del(&tree, key);
+}
+
+int kvstore_rbtree_mod(char *key, char *value)
+{
+    return rbtree_mod(&tree, key, value);
+}
+
+int kvstore_rbtree_count(void)
+{
+    return rbtree_count(&tree);
+}
+
+#endif
 
 // 用于分割提取发来的数据，如 SET KEY VALUE
 int kvstore_split_token(char *msg, char **tokens)
@@ -58,7 +96,7 @@ int kvstore_parser_protocol(connection_t *item, char **tokens, int count)
         return -1;
 
     int cmd;
-    for (cmd = KVS_CMD_START; cmd < KVS_CMD_COUNT; cmd++)
+    for (cmd = KVS_CMD_START; cmd < KVS_CMD_END; cmd++)
     {
         if (strcmp(commands[cmd], tokens[0]) == 0)
             break;
@@ -66,8 +104,10 @@ int kvstore_parser_protocol(connection_t *item, char **tokens, int count)
 
     char *msg = item->wbuffer;
     memset(msg, 0, BUFFER_LENGTH);
+
     switch (cmd)
     {
+    /* 数组结构对应的操作 */
     case KVS_CMD_SET:
     {
         int ret = kvstore_array_set(tokens[1], tokens[2]);
@@ -138,10 +178,102 @@ int kvstore_parser_protocol(connection_t *item, char **tokens, int count)
         }
         break;
     }
+    case KVS_CMD_COUNT:
+    {
+        int ret = kvstore_array_count();
+        if (ret < 0)
+        {
+            LOG("--- ERROR ---\n\n");
+            snprintf(msg, BUFFER_LENGTH, "ERROR");
+        }
+        else
+        {
+            LOG("--- COUNT: %d ---\n\n", ret);
+            snprintf(msg, BUFFER_LENGTH, "%d", ret);
+        }
+        break;
+    }
+
+    /* 红黑树结构对应的操作 */
+    case KVS_CMD_RSET:
+    {
+        int ret = kvstore_rbtree_set(tokens[1], tokens[2]);
+        if (ret == 0)
+        {
+            LOG("--- RSET: %d ---\n\n", ret);
+            snprintf(msg, BUFFER_LENGTH, "SUCCESS");
+        }
+        else if (ret == -1)
+        {
+            LOG("--- ERROR ---\n\n");
+            snprintf(msg, BUFFER_LENGTH, "ERROR");
+        }
+        break;
+    }
+    case KVS_CMD_RGET:
+    {
+        char *value = kvstore_rbtree_get(tokens[1]);
+        if (value)
+        {
+            LOG("---The value of %s is %s ---\n\n", tokens[1], value);
+            snprintf(msg, BUFFER_LENGTH, "%s", value);
+        }
+        else
+        {
+            LOG("--- ERROR ---\n\n");
+            snprintf(msg, BUFFER_LENGTH, "ERROR");
+        }
+        break;
+    }
+    case KVS_CMD_RDEL:
+    {
+        int ret = kvstore_rbtree_del(tokens[1]);
+        if (ret == 0)
+        {
+            LOG("--- DEL %d ---\n\n", ret);
+            snprintf(msg, BUFFER_LENGTH, "SUCCESS");
+        }
+        else if (ret == -1)
+        {
+            LOG("--- ERROR ---\n\n");
+            snprintf(msg, BUFFER_LENGTH, "ERROR");
+        }
+        break;
+    }
+    case KVS_CMD_RMOD:
+    {
+        int ret = kvstore_rbtree_mod(tokens[1], tokens[2]);
+        if (ret == 0)
+        {
+            LOG("--- MOD %d ---\n\n", ret);
+            snprintf(msg, BUFFER_LENGTH, "SUCCESS");
+        }
+        else if (ret == -1)
+        {
+            LOG("--- NO EXIST ---\n\n");
+            snprintf(msg, BUFFER_LENGTH, "NO EXIST");
+        }
+        break;
+    }
+    case KVS_CMD_RCOUNT:
+    {
+        int ret = kvstore_rbtree_count();
+        if (ret < 0)
+        {
+            LOG("--- ERROR ---\n\n");
+            snprintf(msg, BUFFER_LENGTH, "ERROR");
+        }
+        else
+        {
+            LOG("--- RCOUNT: %d ---\n\n", ret);
+            snprintf(msg, BUFFER_LENGTH, "%d", ret);
+        }
+        break;
+    }
     default:
     {
-        LOG("cmd: %s\n", commands[cmd]);
-        assert(0);
+        LOG("CMD: %s 无匹配\n\n", commands[cmd]);
+        // assert(0);
     }
     }
 
@@ -167,8 +299,26 @@ int kvstore_request(connection_t *item)
     return 0;
 }
 
+int init_kvengine(void)
+{
+#if ENABLE_ARRAY_KVENGINE
+#endif
+
+#if ENABLE_RBTREE_KVENGINE
+    int ret = rbtree_create(&tree);
+    if (ret == 0)
+        return 0;
+#endif
+    LOG("Error: 未启用任何存储引擎\n");
+    return -1;
+}
+
 int main(int argc, const char **argv)
 {
+    // 存储组件
+    init_kvengine();
+
+// 网络组件
 #if (ENABLE_NETWORK_SELECT == NETWORK_EPOLL)
     epoll_entry();
 #elif (ENABLE_NETWORK_SELECT == NETWORK_NTYCO)
